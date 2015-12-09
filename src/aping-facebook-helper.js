bcefc753ff40078e34be136c86e04ef6985ae4cb
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-12-09 17:08:03 +0100

    impr: added new helper functions

--- src/aping-facebook-helper.js
+++ src/aping-facebook-helper.js
@@ -14,6 +14,28 @@ jjtApingFacebook.service('apingFacebookHelper', ['apingModels', 'apingTimeHelper
     this.getThisPlattformLink = function () {
         return "https://facebook.com/";
     };
+
+    this.getYoutubeIdFromUrl = function (_url) {
+        var rx = /^.*(?:(?:youtu\.be\/|v\/|vi\/|u\/\w\/|embed\/)|(?:(?:watch)?\?v(?:i)?=|\&v(?:i)?=))([^#\&\?]*).*/;
+        return _url.match(rx)[1] || false;
+    };
+
+    this.getYoutubeImageFromId = function (_youtubeId, size) {
+        switch (size) {
+            case 'default':
+            case 'maxresdefault':
+            case 'mqdefault':
+            case 'sddefault':
+                return "https://img.youtube.com/vi/" + _youtubeId + "/" + size + ".jpg";
+                break;
+
+            case 'hqdefault':
+            default:
+                return "https://img.youtube.com/vi/" + _youtubeId + "/hqdefault.jpg";
+                break;
+        }
+    };
+
     this.getObjectByJsonData = function (_data, _model) {
         var requestResults = [];
         if (_data) {
