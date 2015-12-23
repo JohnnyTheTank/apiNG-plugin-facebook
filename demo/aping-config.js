37416735ba6228e2029f1b5a498020e2de398e70
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-12-23 18:52:14 +0100

    updated config

--- demo/aping-config.js
+++ demo/aping-config.js
@@ -3,8 +3,7 @@ apingApp.config(['$provide', function ($provide) {
 
     $provide.constant("apingApiKeys", {
         facebook: [
-            //{'access_token':'<YOUR_FACBEOOK_ACCESS_TOKEN>'},
-            {'access_token':'CAAMC6AyvDH0BAChyXyiSXINl6nAZCaOP0nXS1T67ngaZA0svefRAJyWx3Y5bzic47wE4iZAMHCAAoQCvhUMZB3SQufDyio9g4vZAj5BgQXuMVDjVvqDrajfm7IEvN0U1O2JfC1FA2VfqpnYgRa9fF5ZAQBW4vHc6ZAmXryyJvzPQd5TkIdqUC4n'},
+            {'access_token':'<YOUR_FACBEOOK_ACCESS_TOKEN>'},
         ],
     });
 
